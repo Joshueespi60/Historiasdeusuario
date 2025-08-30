9ab60abf07e0a28ad9b992b0a826316f930e1e0c
Joshueespi60 <espinaladony95@gmail.com>
2025-08-30 17:50:01 -0500

    Se añadio descripciones

--- labelsdisenos/disenodereck.php
+++ labelsdisenos/disenodereck.php
@@ -31,31 +31,31 @@
 
                 <div class="card card-design-interface" style="background-color: lightblue;">
                     <img src="../imagenes/3.png" alt="Diseño de interfaz de login">
-                    <div class="card-description">Un diseño de interfaz simple y moderno para la página de inicio de sesión.</div>
+                    <div class="card-description">Perfil de vendedor universitario</div>
                     <div class="card-author">Autor: VERA DERECK</div>
                 </div>
 
                 <div class="card card-design-interface">
                     <img src="../imagenes/4.png" alt="Diseño de la página principal">
-                    <div class="card-description">Página principal con un enfoque minimalista para mejorar la navegación.</div>
+                    <div class="card-description">Publicación de productos</div>
                     <div class="card-author">Autor: VERA DERECK</div>
                 </div>
 
                 <div class="card card-design-interface">
                     <img src="../imagenes/5.png" alt="Diseño de la página de productos">
-                    <div class="card-description">Diseño de la galería de productos, optimizado para la búsqueda por categorías.</div>
+                    <div class="card-description">Chat interno seguro</div>
                     <div class="card-author">Autor: VERA DERECK</div>
                 </div>
 
                 <div class="card card-design-interface">
                     <img src="../imagenes/6.png" alt="Diseño de carrito de compras">
-                    <div class="card-description">Una vista detallada del carrito de compras, con opciones de pago seguras.</div>
+                    <div class="card-description">Sistema de valoraciones</div>
                     <div class="card-author">Autor: VERA DERECK</div>
                 </div>
 
                 <div class="card card-design-interface">
                     <img src="../imagenes/7.png" alt="Diseño del perfil de usuario">
-                    <div class="card-description">Interfaz del perfil de usuario con acceso rápido a su historial y configuraciones.</div>
+                    <div class="card-description">Puntos de entrega dentro de la universidad</div>
                     <div class="card-author">Autor: VERA DERECK</div>
                 </div>
 
